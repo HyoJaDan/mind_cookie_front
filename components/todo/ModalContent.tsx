585daae92c56f7f2c15f262bb3ca8548735ac02d
전성호 <kopasd99@gmail.com>
2024-10-02 16:29:46 +0900

    style : picker을 삭제하고 스타일 만들기

--- components/todo/ModalContent.tsx
+++ components/todo/ModalContent.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-import RNPickerSelect from "react-native-picker-select";
 import { Colors } from "../../assets/color/color";
 import { fontStyle } from "../../assets/font/font";
 import { PrimaryHobbit } from "../../data/todo";
@@ -41,62 +41,72 @@ const ModalContent: React.FC<ModalContentProps> = ({
 }) => {
   return (
     <View style={styles.modalContainer}>
-      <View style={styles.modalContent}>
-        <RNPickerSelect
-          onValueChange={(itemValue) => {
-            setNewPrimaryHobbit("");
-            setSelectedPrimaryHobbit(itemValue);
-          }}
-          items={todos.map((primaryHobbit) => ({
-            label: primaryHobbit.primaryHobbit,
-            value: primaryHobbit.primaryHobbit,
-            key: primaryHobbit.primaryHobbitId,
-          }))}
-          placeholder={{ label: "기존 상위 목표 선택", value: "" }}
-          style={pickerSelectStyles}
-        />
-        <TextInput
-          placeholder="새로운 상위 목표 입력"
-          style={styles.input}
-          value={newPrimaryHobbit}
-          onChangeText={(text) => {
-            setNewPrimaryHobbit(text);
-            setSelectedPrimaryHobbit("");
-          }}
-        />
-        <TextInput
-          placeholder="세부 목표 입력"
-          style={styles.input}
-          value={newHobbit}
-          onChangeText={setNewHobbit}
-        />
-        <Text style={[fontStyle.SB12, styles.inputNav]}>
-          *기존 상위 목표나, 새로운 상위 목표중 하나만 선택할수 있습니다.
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text style={[fontStyle.BD24, { marginBottom: 20 }]}>
+          목표를 추가하세요
         </Text>
-        {newPrimaryHobbit !== "" && (
-          <View>
-            <Text style={[fontStyle.MD16, { marginBottom: 10 }]}>
-              고유색 선택
-            </Text>
-            <View style={styles.colorPickerContainer}>
-              {colorOptions.map((color, index) => (
-                <TouchableOpacity
-                  key={index}
-                  style={[
-                    styles.colorCircle,
-                    {
-                      backgroundColor: color,
-                      borderColor:
-                        selectedColor === color ? "#000" : "transparent",
-                    },
-                  ]}
-                  onPress={() => setSelectedColor(color)}
-                />
-              ))}
-            </View>
+        <Text style={fontStyle.BD16}>상위 목표</Text>
+        <View style={styles.modalContent}>
+          <TextInput
+            placeholder="새로운 상위 목표 입력"
+            style={styles.newPrimaryHobbitInput}
+            value={newPrimaryHobbit}
+            onChangeText={(text) => {
+              setNewPrimaryHobbit(text);
+              setSelectedPrimaryHobbit("");
+            }}
+          />
+          <View style={styles.existingGoalsContainer}>
+            {todos.map((primaryHobbit) => (
+              <TouchableOpacity
+                key={primaryHobbit.primaryHobbitId}
+                style={[
+                  styles.existingGoalItem,
+                  selectedPrimaryHobbit === primaryHobbit.primaryHobbit
+                    ? styles.selectedGoalItem
+                    : { backgroundColor: Colors.grayscale.gray400 },
+                ]}
+                onPress={() => {
+                  setNewPrimaryHobbit(primaryHobbit.primaryHobbit);
+                  setSelectedPrimaryHobbit(primaryHobbit.primaryHobbit);
+                }}
+              >
+                <Text style={styles.existingGoalText}>
+                  {primaryHobbit.primaryHobbit}
+                </Text>
+              </TouchableOpacity>
+            ))}
           </View>
-        )}
-      </View>
+          <Text style={fontStyle.BD16}>세부 목표</Text>
+          <TextInput
+            placeholder="세부 목표 입력"
+            style={styles.input}
+            value={newHobbit}
+            onChangeText={setNewHobbit}
+          />
+          {newPrimaryHobbit !== "" && selectedPrimaryHobbit === "" && (
+            <View style={{ gap: 10, marginVertical: 20 }}>
+              <Text style={fontStyle.BD16}>고유색 선택</Text>
+              <View style={styles.colorPickerContainer}>
+                {colorOptions.map((color, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    style={[
+                      styles.colorCircle,
+                      {
+                        backgroundColor: color,
+                        borderColor:
+                          selectedColor === color ? "#000" : "transparent",
+                      },
+                    ]}
+                    onPress={() => setSelectedColor(color)}
+                  />
+                ))}
+              </View>
+            </View>
+          )}
+        </View>
+      </ScrollView>
       <DefaultButton pressHandler={handleAddTodo} text="새로운 목표 추가" />
     </View>
   );
@@ -106,10 +116,21 @@ const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
     justifyContent: "space-between",
-    alignItems: "center",
     padding: 20,
   },
-  modalContent: { alignItems: "center", width: "100%", gap: 20 },
+  content: {
+    width: "100%",
+  },
+  modalContent: {
+    width: "100%",
+    gap: 10,
+  },
+  newPrimaryHobbitInput: {
+    width: "100%",
+    borderBottomWidth: 2,
+    borderBottomColor: "#0000FF",
+    paddingVertical: 10,
+  },
   input: {
     width: "100%",
     borderWidth: 1,
@@ -117,7 +138,28 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
   },
-  colorPickerContainer: { flexDirection: "row", flexWrap: "wrap" },
+  existingGoalsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    marginBottom: 20,
+  },
+  existingGoalItem: {
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  selectedGoalItem: {
+    backgroundColor: "#d0e8ff", // 선택된 항목의 배경색
+  },
+  existingGoalText: {
+    color: "#333",
+  },
+  colorPickerContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
   colorCircle: {
     width: 40,
     height: 40,
@@ -129,31 +171,10 @@ const styles = StyleSheet.create({
     color: Colors.grayscale.gray800,
     textAlign: "right",
     width: "100%",
-    position: "relative",
-    top: -10,
+    marginTop: 10,
   },
-});
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30,
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30,
+  scrollViewContent: {
+    flexGrow: 1, // ScrollView의 전체 콘텐츠가 채워지도록 설정
   },
 });
 
